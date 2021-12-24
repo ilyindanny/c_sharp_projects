f3ec85d20d632979e117cb9a65acf0470d2d6484
ilyindanny <ilyindanny@gmail.com>
2021-12-24 19:56:35 +0300

    example 003 on branch 002

--- Lessons/Lesson_002/Example_002/Program.cs
+++ Lessons/Lesson_002/Example_002/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// New method with array and functions:
+
+/*
+int [] array = {9, 8, 7, 67, 5, 46, 3, 2, 1};
+
+int FindMaxMethod (int a, int b, int c)
+{
+    int result = a;
+    if (b > result) result = b;
+    if (c > result) result = c;
+    return result;
+}
+
+int max = FindMaxMethod(
+    FindMaxMethod(array[0], array[1], array[2]), FindMaxMethod(array[3], array[4], array[5]), FindMaxMethod(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
+*/
+
+
+// More good method:
+
+
+int [] array = {9, 8, 7, 67, 5, 46, 3, 2, 1};
+
+int n = array.Length;
+int FindSomeMyDigit = 1;
+int ArrayElementsCounter = 0;
+
+while (ArrayElementsCounter < n)
+{
+    if (array [ArrayElementsCounter] == find)
+    {
+        Console.WriteLine(ArrayElementsCounter);
+    }
+    ArrayElementsCounter++;
+}
---
+++ Lessons/Lesson_002/Example_003/Program.cs
@@ -0,0 +1,33 @@
+﻿// More information about array:
+
+
+
+void FillArray (int [] AnyArrayName)
+{
+    int length = AnyArrayName.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        AnyArrayName[index] = new Random().Next(1, 1440);
+        index++;
+    }
+}
+
+
+void PrintArray (int [] OtherAnyArrayName)
+{
+    int count = OtherAnyArrayName.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.WriteLine(OtherAnyArrayName[position]);
+        position++;
+    }
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
