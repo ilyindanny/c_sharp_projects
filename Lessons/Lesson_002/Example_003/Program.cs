509595b5fa7434ee23dafa8053dcd10d2b7f7c47
ilyindanny <ilyindanny@gmail.com>
2021-12-24 21:02:35 +0300

    lesson 002 finished

--- Lessons/Lesson_002/Example_003/Program.cs
+++ Lessons/Lesson_002/Example_003/Program.cs
@@ -1,33 +1,66 @@
-﻿// More information about array:
+﻿// More information about array and methods:
 
 
+// Method to put sume cosuale numbers to array.
 
-void FillArray (int [] AnyArrayName)
+void FillArray(int[] NameArray)
 {
-    int length = AnyArrayName.Length;
+    int length = NameArray.Length;
     int index = 0;
 
     while (index < length)
     {
-        AnyArrayName[index] = new Random().Next(1, 1440);
+        NameArray[index] = new Random().Next(1, 10);
         index++;
     }
 }
 
 
-void PrintArray (int [] OtherAnyArrayName)
+void PrintArray(int[] NameArray)
 {
-    int count = OtherAnyArrayName.Length;
+    int count = NameArray.Length;
     int position = 0;
 
     while (position < count)
     {
-        Console.WriteLine(OtherAnyArrayName[position]);
+        Console.WriteLine(NameArray[position]);
         position++;
     }
 }
 
-int [] array = new int [10];
+
+int IndexOf(int[] NameArray, int NumberWeFind)
+{
+    int length = NameArray.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < length)
+    {
+        if (NameArray[index] == NumberWeFind)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+int NumberThatWeFind = 7;
 
 FillArray(array);
+
 PrintArray(array);
+
+Console.WriteLine();
+
+int answer = IndexOf(array, NumberThatWeFind);
+
+Console.Write("your number \""); Console.Write(NumberThatWeFind); Console.Write("\" is in this position: ");
+Console.Write(answer);
+Console.WriteLine();
+Console.WriteLine();
+
+
