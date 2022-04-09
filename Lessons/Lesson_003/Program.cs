62afd910f0fdd9ffb0ca81b4ca4a303c83f711a8
ilyin Danny <ilyindanny@gmail.com>
2022-04-09 12:20:50 +0300

    new lessons

---
+++ Lessons/Lesson_003/Program.cs
@@ -0,0 +1,24 @@
+﻿// for
+
+
+/* 
+
+
+for (int x = 1; x < 10; x++)
+{
+    for (int y = 1; y < 10; y++)
+    {
+        Console.WriteLine($"{x} * {y} = {x * y}");
+    }
+    Console.WriteLine();
+
+}
+
+
+string a = "12345";
+int b = a.Length;
+Console.WriteLine(b);
+
+*/
+
+
