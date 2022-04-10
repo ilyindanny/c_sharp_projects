cf20e583bdf031ee322e1339c09b0dec67e00055
ilyin Danny <ilyindanny@gmail.com>
2022-04-10 22:32:06 +0300

    after first lesson all had done +004

---
+++ NewLessons/Lesson_001/007/Program.cs
@@ -0,0 +1,16 @@
+﻿// hello Vika!
+
+Console.WriteLine("type your name: ");
+
+string name = Console.ReadLine();
+
+
+if (name.ToLower() == "victoria")
+{
+Console.WriteLine($"Hello dear Vika!");
+}
+else
+{
+    Console.WriteLine($"Hi {name}");
+}
+
