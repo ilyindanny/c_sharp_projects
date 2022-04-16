e8b701ce284b450f04551d2885f30087992d9aba
ilyin Danny <ilyindanny@gmail.com>
2022-04-16 23:50:30 +0300

    HomeWork after seminar 2

---
+++ NewHomeWork/Seminar002/HomeWork/001/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+
+*/
+
+
+
+using System;
+using static System.Console;
+
+
+
+// the first way
+
+
+int result01 = Convert.ToInt32(Console.ReadLine());
+
+result01 = result01 / 10 % 10;
+
+Console.WriteLine(result01);
+
+
+
+// the second way
+
+
+string result02 = Console.ReadLine();
+
+Console.WriteLine(result02[1]);
+
+
+
+// the third way (if you find out the second digit from tne end)
+
+
+string result002 = Console.ReadLine();
+
+int resultLength = result002.Length;
+
+Console.WriteLine(result002[resultLength - 2]);
+
+
+
+// the 4th way (if you wanna convert a string to an integer)
+
+
+int result03 = Convert.ToInt32(Console.ReadLine()[1].ToString());
+
+
+Console.WriteLine(result03);
---
+++ NewHomeWork/Seminar002/HomeWork/002/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+*/
+
+
+
+using System;
+using static System.Console;
+
+
+
+// if you wanna find out the third digit from tne beginning
+
+
+string result04 = Console.ReadLine();
+
+int length = result04.Length;
+
+if (length < 3) Console.WriteLine("there isn't the third digit");
+else
+{
+	Console.WriteLine(result04[2]);
+}
+
+
+
+// if you wanna find out the third digit from tne end
+
+
+string result004 = Console.ReadLine();
+
+int length01 = result004.Length;
+
+if (length01 < 3) Console.WriteLine("there isn't the third digit");
+else
+{
+	Console.WriteLine(result004[length01 - 3]);
+}
