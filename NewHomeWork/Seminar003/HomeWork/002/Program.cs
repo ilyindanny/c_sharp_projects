4e164b5caf2dc45380505d1ac7e8bba63731cf98
ilyin Danny <ilyindanny@gmail.com>
2022-04-19 17:58:50 +0300

    Home work after seminar 3

--- NewHomeWork/Seminar003/HomeWork/002/Program.cs
+++ NewHomeWork/Seminar003/HomeWork/002/Program.cs
@@ -9,30 +9,15 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 
+Console.WriteLine("input coordonates with space: x1 y1 z1 x2 y2 z2");
+string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-Console.WriteLine("imput coordinate x1");
-
-double x1 = Convert.ToDouble(Console.ReadLine() ?? "0");
-
-Console.WriteLine("imput coordinate y1");
-
-double y1 = Convert.ToDouble(Console.ReadLine() ?? "0");
-
-Console.WriteLine("imput coordinate z1");
-
-double z1 = Convert.ToDouble(Console.ReadLine() ?? "0");
-
-Console.WriteLine("imput coordinate x1");
-
-double x2 = Convert.ToDouble(Console.ReadLine() ?? "0");
-
-Console.WriteLine("imput coordinate y1");
-
-double y2 = Convert.ToDouble(Console.ReadLine() ?? "0");
-
-Console.WriteLine("imput coordinate z1");
-
-double z2 = Convert.ToDouble(Console.ReadLine() ?? "0");
+double x1 = Convert.ToDouble(array[0]);
+double y1 = Convert.ToDouble(array[1]);
+double z1 = Convert.ToDouble(array[2]);
+double x2 = Convert.ToDouble(array[3]);
+double y2 = Convert.ToDouble(array[4]);
+double z2 = Convert.ToDouble(array[5]);
 
 
 
