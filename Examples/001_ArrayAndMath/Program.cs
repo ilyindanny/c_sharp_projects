6954d284cdb0096e0b2ea828129fef15266a09e4
ilyin Danny <ilyindanny@gmail.com>
2022-04-19 18:50:57 +0300

    Examples has added

--- Examples/001_ArrayAndMath/Program.cs
+++ Examples/001_ArrayAndMath/Program.cs
@@ -1,4 +1,5 @@
-﻿//
+﻿// study
+
 /*
 Задача 21
 
@@ -8,6 +9,33 @@ A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
+using System;
+using static System.Console;
+
+
+void ConvertArrayToDouble(string[] array01, double[] array02)
+{
+	int length = array01.Length;
+	for (int index = 0; index < length; index += 1)
+	{
+		array02[index] = Convert.ToDouble(array01[index]);
+	}
+}
+
+
+WriteLine("input coordonates with space: x1 y1 z1 x2 y2 z2");
+string[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double[] a = new double[6];
+
+ConvertArrayToDouble(array, a);
+
+double resoult = Math.Sqrt(Math.Pow(a[4] - a[1], 2) + Math.Pow(Math.Sqrt(Math.Pow(a[3] - a[0], 2) + Math.Pow(a[5] - a[2], 2)), 2));
+
+WriteLine($"{resoult:N2}");
+
+
+/*
+// Alternative:
 
 Console.WriteLine("input coordonates with space: x1 y1 z1 x2 y2 z2");
 string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
@@ -27,3 +55,4 @@ double z2 = Convert.ToDouble(array[5]);
 double resoult = Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(z2 - z1, 2)), 2));
 
 Console.WriteLine($"{resoult:N2}");
+*/
