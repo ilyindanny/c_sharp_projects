7bd8d44ec1d77b411be7edd298c3b4cf779b0c6b
Danny <ilyindanny@gmail.com>
2022-04-27 12:52:57 +0300

    Update //33

--- NewHomeWork/Seminar005/001/Program.cs
+++ NewHomeWork/Seminar005/001/Program.cs
@@ -41,5 +41,4 @@ int[] GetArray(int size, int minValue, int maxValue)
 // int[] k=new int[20].Select(x=>new Random().Next(0,1000)).Where(x=>x>=9&&x<=100).ToArray();
 // Console.WriteLine(String.Join(" ",k));
 // text
-//4
-
+//33
