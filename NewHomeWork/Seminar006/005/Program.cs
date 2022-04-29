72e2e8c07a2a448065a0cbbaf5015815b299984a
ilyin Danny <ilyindanny@gmail.com>
2022-04-29 08:58:21 +0300

    temp

---
+++ NewHomeWork/Seminar006/005/Program.cs
@@ -0,0 +1,17 @@
+﻿//
+/*
+Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+*/
+
+using System;
+using static System.Console;
+Clear();
+
+int[] array1 = { 1, 2, 3, 4, 5 };
+int[] array2 = new int[array1.Length];
+
+for (int i = 0; i < array1.Length; i++)
+{
+	array2[i] = array1[i];
+	Write($"{array1[i]} -> {array2[i]} ");
+}
---
+++ NewHomeWork/Seminar006/006/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a1 = int.Parse(Console.ReadLine());
+int a2 = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+От Roman Bachinsky всем 09:59 PM
+с подсказками
+тяжеловато
+От Олег Канакаев всем 09:59 PM
+int F1 = int.Parse(Console.ReadLine());
+int F2 = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+GetFibonacci(F1, F2, N);
+
+void GetFibonacci(int F1, int F2, int N)
+{
+	if (N >= 1)
+		Console.Write(F1 + " ");
+	if (N >= 2)
+		Console.Write(F2 + " ");
+	for (int i = 0; i < N - 2; i++)
+	{
+		F2 += F1;
+		F1 = F2 - F1;
+		Console.Write(F2 + " ");
+	}
+}
