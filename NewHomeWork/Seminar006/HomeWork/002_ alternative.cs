0f7d46f631d56d8d6a55448882d999b31527d2d4
Danny <ilyindanny@gmail.com>
2022-04-29 12:23:21 +0300

    HomeWork after seminar 6

--- NewHomeWork/Seminar006/HomeWork/002_ alternative.cs
+++ NewHomeWork/Seminar006/HomeWork/002_ alternative.cs
@@ -19,10 +19,43 @@ double b1 = 2;
 double k2 = 9;
 double b2 = 4;
 
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
+double x1 = 0;
+double x2 = 0;
 
-WriteLine($"{x}, {y}");
- 
+double Ny = 0;
+double My = 0;
+
+
+FindXY();
+
+void FindXY()
+{
+	string result = "nothing";
+	for (double i = 0; i < 10000000; i++)
+	{
+	Ny = k1 * x1 + b1;
+	My = k2 * x1 + b2;
+	
+	if (Ny == My)
+	{
+		result = $"X {x1:N1}, Y {Ny}";
+		break;
+	}
+	x1 += 0.1;
+	
+	Ny = Math.Round(k1 * x2 + b1, 1);
+	My = Math.Round(k2 * x2 + b2, 1);
+	
+	if (Ny == My)
+	{
+		result = $"X {x2:N1}, Y {Ny}";
+		break;
+	}
+	x2 -= 0.1;
+	
+	}
+	Write(result);	
+}
+
     }
 }
