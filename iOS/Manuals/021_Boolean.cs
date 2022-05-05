c4a8fdd30988f030cd4007ac2835f29eac6a4065
Danny <ilyindanny@gmail.com>
2022-05-05 23:37:05 +0300

    Add 021_Boolean.cs

---
+++ iOS/Manuals/021_Boolean.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Program
+{
+    static public void Main ()
+    {
+
+// тип Boolean принимает значения true или fulse. в выражениях используются операторы ИЛИ "&", И "|", ИЛИ НЕ "^", НЕ "!".
+
+// выражение dool b = (a | b) & !(a | b); заменяет оператор ИЛИ НЕ.
+
+// выражение bool bToggle ^= true; всегда меняет значение bToggle;
+
+// массив заполняется значением fulse
+
+// b = Boolean.Parse(str); сравнивает содержимое строки с true и fulse независимо от регистра
+
+// b.ToString() возвращает слова True или Fulse
+
+// Convert.ToBolean() возвращает fulse, есои значение ноль, и true, если не ноль
+
+// Convert.ToDecimal() в обратном направлении конвертирует true в 1, а fulse в 0
+
+
+
+
+
+    }
+}
