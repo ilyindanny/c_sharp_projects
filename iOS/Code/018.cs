ff2563df8eb3effaab77c18f642d2d1f034ba15b
Danny <ilyindanny@gmail.com>
2022-05-05 19:32:30 +0300

    Update 011.cs, 018.cs and two other files

--- iOS/Code/018.cs
+++ iOS/Code/018.cs
@@ -1,9 +1,21 @@
 using System;
 
+using static System.Console;
+
 public class Program
 {
     static public void Main ()
     {
-        
+
+int i = new Random().Next(1, 5);
+
+WriteLine(new string[] {"a", "b", "c", "d"} [i - 1]);
+
+
+int[,,,] test = new int[5,5,5,5];
+
+test[2,2,2,2] = 555;
+WriteLine(test[2,2,2,2] + " " + test.Length + " " + test.GetLength(3));
+
     }
 }
