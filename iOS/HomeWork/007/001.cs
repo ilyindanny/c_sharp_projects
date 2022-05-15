746b6cb48f86840368966f5e3c05854f02f77cb9
Danny <ilyindanny@gmail.com>
2022-05-15 23:34:39 +0300

    Update 001.cs, 002.cs and one other file

--- iOS/HomeWork/007/001.cs
+++ iOS/HomeWork/007/001.cs
@@ -8,36 +8,51 @@ public class Program
 {
     static public void Main ()
     {
-// // написать метод, создающий двумерный массив и заполняющий его случайными числами
 
-int[,] array = GetDoubleArray(5, 5);
-WriteLine(array[2,2]);
+/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
 
 
-int[,] GetDoubleArray(int x, int y)
+int m = int.Parse(ReadLine());
+int n = int.Parse(ReadLine());
+
+double[,] array = GetArray(m, n);
+PrintArray(array);
+
+
+
+double[,] GetArray(int rows, int columns)
 {
-	int[,] array = new int[x, y];
-	for (int i = 0; i < x -1; i++)
+	double[,] array = new double[rows, columns];
+	for(int i = 0; i < rows; i++)
 	{
-		for (int j = 0; j < y -1; j++)
+		for(int j = 0; j < columns; j++)
 		{
-			array[i, j] = new Random().Next(10);
+			// array[i, j] = Convert.ToDouble(new Random().Next(-99, 100)) / 10;
+			
+			array[i, j] = new Random().Next(2) == 0 
+			? Convert.ToDouble(new Random().Next(-99, 100)) / 10 
+			: Convert.ToDouble(new Random().Next(-9, 10));
 		}
 	}
 	return array;
 }
 
 
-int[,] GetDoubleArray02(int x, int y)
+void PrintArray(double[,] array)
 {
-	int[,] array = new int[x, y].Select(x => new Random().Next(10)).Select(y => new Random().Next(10)).ToArray();
-	return array;
+	for(int i = 0; i < array.GetLength(0); i++)
+	{
+		for(int j = 0; j < array.GetLength(1); j++)
+		{
+			Write("{0, 5}", array[i, j]);
+		}
+		WriteLine();
+	}
 }
-
-
-
-
-
-
     }
 }
---
+++ iOS/HomeWork/007/003.cs
@@ -0,0 +1,68 @@
+using System;
+
+using static System.Console;
+
+public class Program
+{
+    static public void Main ()
+    {
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int[,] array = GetArray(5, 5);
+
+PrintArray(array);
+WriteLine("\nСреднее арифметическое каждого столбца:");
+FindAverage(array);
+
+
+int[,] GetArray(int rows, int columns)
+{
+	int[,] array = new int[rows, columns];
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < columns; j++)
+		{
+			array[i, j] = new Random().Next(10);
+		}
+	}
+	return array;
+}
+
+
+void FindAverage(int[,] array)
+{
+	double sum = 0;
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+		for (int i = 0; i < array.GetLength(0); i++)
+		{
+			sum += Convert.ToDouble(array[i, j]);
+		}
+		Write("{0, 5}", sum / array.GetLength(0));
+		sum = 0;
+	}
+	
+}
+
+
+void PrintArray(int[,] array)
+{
+	for(int i = 0; i < array.GetLength(0); i++)
+	{
+		for(int j = 0; j < array.GetLength(1); j++)
+		{
+			Write("{0, 5}", array[i, j]);
+		}
+		WriteLine();
+	}
+}
+    }
+}
