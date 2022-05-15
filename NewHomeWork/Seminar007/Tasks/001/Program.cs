48d1314620da1f55e5fd89239bc1398d616b3207
ilyin Danny <ilyindanny@gmail.com>
2022-05-15 12:05:19 +0300

    temp

--- NewHomeWork/Seminar007/Tasks/001/Program.cs
+++ NewHomeWork/Seminar007/Tasks/001/Program.cs
@@ -2,16 +2,17 @@
 using System;
 using static System.Console;
 using System.Linq;
+using MyClasses;
 class Program
 {
 	static void Main()
 	{
 
-		int[,] array = MyClasses.MyExample.GetArray(5, 5, 1, 100);
+		int[,] array = MyExample.GetArray(5, 5, 1, 100);
 
 		WriteLine(array[1, 1]);
 
-		MyClasses.MyExample.PrintArray(array);
+		MyExample.PrintArray(array);
 
 
 	}
