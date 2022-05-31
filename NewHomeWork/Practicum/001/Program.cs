f980de29e876747dbf79c760bb3bd25fd1714447
ilyin Danny <ilyindanny@gmail.com>
2022-05-31 22:36:00 +0300

    temp

---
+++ NewHomeWork/Practicum/001/Program.cs
@@ -0,0 +1,12 @@
+﻿// сформируйте массив из квадратов значений заданного массива
+
+using System;
+using static System.Console;
+using System.Linq;
+class Program
+{
+	static void Main()
+	{
+
+	}
+}
