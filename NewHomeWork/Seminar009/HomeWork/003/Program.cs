ff4431e8a694e0e01aa53721f5a305f0f75bb27f
ilyin Danny <ilyindanny@gmail.com>
2022-05-31 22:56:02 +0300

    homework after seminar 9

---
+++ NewHomeWork/Seminar009/HomeWork/003/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29
+
+
+из интернета:
+
+		 / n + 1 если m = 0
+		 |
+F(m, n)< F(m - 1, 1) если m != 0 и n = 0
+		 |
+		 \ F(m - 1, F(m, n - 1)) если m и n больше нуля
+*/
+
+using System;
+using static System.Console;
+public class Program
+{
+	static public void Main()
+	{
+
+		Write(GetFunction(3, 2));
+
+		int GetFunction(int m, int n)
+		{
+			return m == 0 ? n + 1 : m != 0 && n == 0 ? GetFunction(m - 1, 1) : GetFunction(m - 1, GetFunction(m, n - 1));
+		}
+
+	}
+}
